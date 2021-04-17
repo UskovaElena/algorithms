f401aaee62651eeae2489a6f811e7a9cb570c985
Lukicheva Polina <63358667+LukichevaPolina@users.noreply.github.com>
2021-04-17 14:07:00 +0300

    Add files via upload

--- VRPTW/VRPTW.py
+++ VRPTW/VRPTW.py
@@ -1,12 +1,7 @@
+import math
 class Shop:
     def __init__(self, a):
-        self.id = a[0]
-        self.x = a[1]
-        self.y = a[2]
-        self.demand = a[3]
-        self.w_start = a[4]
-        self.w_end = a[5]
-        self.unloading = a[6]
+        self.id, self.x, self.y, self.demand, self.w_start, self.w_end, self.unloading = a
 
 
 class VRPTW:
@@ -15,12 +10,26 @@ class VRPTW:
         self.shops = []
         self.n_vehicles = 0
         self.capacity_vehicles = 0
+        self.distances = []
 
-    def read_file(self, path):
+    # read file and fill fields of class
+    def fill(self, path):
         with open(path) as f:
             self.n_vehicles, self.capacity_vehicles  = f.readline().replace('\n','').split(' ')[1:]
             for line in f.readlines():
                 self.shops.append(Shop([int(a) for a in line.replace('\n', '').split(' ')[1:-1]]))
 
+        for i in range(len(self.shops) - 1):
+            self.distances.append([])
+            for j in range(i + 1, len(self.shops)):
+                self.distances[i].append(math.sqrt(math.pow(self.shops[i].x - self.shops[j].x, 2) +
+                                                 math.pow(self.shops[i].y - self.shops[j].y, 2)))
+
+    #find first(initial) solution
+    def initial_solution(self):
+        return
+
+
+
 alg = VRPTW()
-alg.read_file("C:/Users/Polina/PycharmProject/VRPTW/I1.txt")
+alg.fill("C:/Users/Polina/PycharmProject/VRPTW/I1.txt")
